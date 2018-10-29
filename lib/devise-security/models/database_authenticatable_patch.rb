f1b3e0ebb23ec7bf95110694bfef452dbea8b394
Fabrizio <frcavada@uc.cl>
2018-10-29 13:50:26 -0300

    Update database_authenticatable_patch.rb
    pruebas de password_digest

--- lib/devise-security/models/database_authenticatable_patch.rb
+++ lib/devise-security/models/database_authenticatable_patch.rb
@@ -23,6 +23,11 @@ module Devise
         clean_up_passwords
         result
       end
+    
+    protected
+      def password_digest(password)
+        Devise::Encryptor.digest(self.class, password)
+      end
     end
   end
 end
